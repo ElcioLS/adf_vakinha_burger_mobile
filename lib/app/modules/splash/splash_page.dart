5d5445e34ae42a983248a561cc6f86a54379543c
ElcioLS <elciols@gmail.com>
2022-11-12 16:54:05 -0300

    criação Button e AppBar

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,3 +1,5 @@
+import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
 import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 import 'package:flutter/material.dart';
 
@@ -7,18 +9,18 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
+      appBar: VakinhaAppbar(),
       body: Column(
-        children: const [
-          SizedBox(height: 10),
-          Padding(
+        children: [
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.all(8.0),
             child: VakinhaTextformfield(
               label: 'Text',
             ),
           ),
+          const SizedBox(height: 20),
+          VakinhaButton(label: 'Entrar', onPressed: () {}),
         ],
       ),
     );
