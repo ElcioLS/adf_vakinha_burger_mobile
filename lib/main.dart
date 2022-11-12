03cd8e1ab10b70fbfd1beb049ac52b02e28c6c29
ElcioLS <elciols@gmail.com>
2022-11-12 00:04:02 -0300

    RestClient preparado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:adf_vakinha_burger_mobile/app/code/ui/vakinha_ui.dart';
+import 'package:adf_vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:adf_vakinha_burger_mobile/app/routes/splash_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
