3838d311b8b168c59afb8d601af317d9bfb8c546
ElcioLS <elciols@gmail.com>
2022-11-12 11:32:13 -0300

    criação TextFormField

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -9,7 +10,17 @@ class SplashPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Splash'),
       ),
-      body: Container(),
+      body: Column(
+        children: const [
+          SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: VakinhaTextformfield(
+              label: 'Text',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
