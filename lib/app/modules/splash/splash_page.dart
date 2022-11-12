c5b8e532e9152d0ecfff6d15b3316fda8d59c0aa
ElcioLS <elciols@gmail.com>
2022-11-12 17:22:52 -0300

    tela de Splash

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,7 +1,6 @@
-import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
 import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
-import 'package:adf_vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,20 +8,39 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: VakinhaAppbar(),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: VakinhaTextformfield(
-              label: 'Text',
-            ),
-          ),
-          const SizedBox(height: 20),
-          VakinhaButton(label: 'Entrar', onPressed: () {}),
-        ],
-      ),
+      body: Container(
+          color: const Color(0xFF140E0E),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: context.width,
+                  child: Image.asset(
+                    'assets/images/lanche.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    // SizedBox(height: MediaQuery.of(context).size.height * .15),
+                    SizedBox(
+                      height: context.heightTransformer(reducedBy: 85),
+                    ),
+                    Image.asset('assets/images/logo.png'),
+                    const SizedBox(height: 40),
+                    VakinhaButton(
+                        label: 'ACESSAR',
+                        width: context.widthTransformer(reducedBy: 40),
+                        onPressed: () {}),
+                  ],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
