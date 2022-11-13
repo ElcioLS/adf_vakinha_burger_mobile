93c44628eb10bf42948cf62a144c4b7ac0d4cf1e
ElcioLS <elciols@gmail.com>
2022-11-13 17:06:43 -0300

    classes utilitárias

--- lib/app/modules/register/register_controller.dart
+++ lib/app/modules/register/register_controller.dart
@@ -1,9 +1,12 @@
 import 'package:adf_vakinha_burger_mobile/app/core/mixins/loader_mixin.dart';
+import 'package:adf_vakinha_burger_mobile/app/core/mixins/messages_mixin.dart';
 import 'package:adf_vakinha_burger_mobile/app/repositories/auth/auth_repository.dart';
 import 'package:get/get.dart';
 
-class RegisterController extends GetxController with LoaderMixin {
+class RegisterController extends GetxController
+    with LoaderMixin, MessagesMixin {
   final AuthRepository _authRepository;
+  final message = Rxn<MessageModel>();
 
   final loading = false.obs;
 
@@ -13,11 +16,22 @@ class RegisterController extends GetxController with LoaderMixin {
   @override
   void onInit() {
     loaderListener(loading);
+    messageListener(message);
     super.onInit();
   }
 
   void qualquer() {
-    loading.toggle();
-    Future.delayed(const Duration(seconds: 2), () => loading.toggle());
+    message.value = MessageModel(
+      title: 'Teste',
+      message: 'Mensagem de teste',
+      type: MessageType.error,
+    );
+    message(MessageModel(
+      title: 'Teste',
+      message: 'Mensagem de info',
+      type: MessageType.info,
+    ));
+    // loading.toggle();
+    // Future.delayed(const Duration(seconds: 2), () => loading.toggle());
   }
 }
