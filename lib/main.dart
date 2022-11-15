38f7d52d8224446165a9f9b6179505cc6d457468
ElcioLS <elciols@gmail.com>
2022-11-15 19:48:27 -0300

    Adicionando GetStorage na inicialização do main

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,17 @@ import 'package:adf_vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:adf_vakinha_burger_mobile/app/routes/splash_routers.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/auth_routers.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await GetStorage.init();
+  runApp(const VakinhaBurgerMainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class VakinhaBurgerMainApp extends StatelessWidget {
+  const VakinhaBurgerMainApp({super.key});
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
